6d1f83a621e188a6f7dd9ce64bcfb69aafd1363e
TusharIbtekar <ibtekar8@gmail.com>
2022-03-30 01:45:26 +0600

    Minor typo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import { useEffect, useState } from "react";
-import { Col, Row, Typography } from "antd";
-import InputForm from "./components/input.component";
-import TodoItem from "./components/todoItem.component";
-import moment from "moment";
+import { useEffect, useState } from 'react';
+import { Col, Row, Typography } from 'antd';
+import moment from 'moment';
+
+import InputForm from './components/input.component';
+import TodoItem from './components/todoItem.component';
 
 function App() {
   const [todos, setTodos] = useState([]);
-  const [doneTodos, setDoneTodos] = useState([])
+  const [doneTodos, setDoneTodos] = useState([]);
 
-  const addTodo = todo => {
-    setTodos([...todos, { id: Date.now(), title: todo, done: false, startTime: moment() }])
-  }
-
-  const handleDelete = id => {
-    const availableTodos = [...todos].filter(todo => id != todo.id)
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: Date.now(), title: todo, done: false, startTime: moment() },
+    ]);
+  };
+  const handleDelete = (id) => {
+    const availableTodos = [...todos].filter((todo) => id !== todo.id);
     setTodos(availableTodos);
-  }
+  };
 
   const handleDone = (id, endTime) => {
     const todoList = [...todos];
@@ -38,53 +41,55 @@ function App() {
     setTodos(todoList);
   };
 
-
-
   const setLocalTodos = () => {
     if (todos.length > 0) {
       // console.log(todos);
-      localStorage.setItem('todos', JSON.stringify(todos))
+      localStorage.setItem('todos', JSON.stringify(todos));
     }
-  }
+  };
 
   const getLocalTodos = () => {
     let todoLocal = JSON.parse(localStorage.getItem('todos'));
-    if (todoLocal && todoLocal.length)
+    if (todoLocal && todoLocal.length) {
       setTodos(todoLocal);
-    console.log('here');
-  }
+      console.log('here');
+      todos.map((todo) =>
+        // todo.done ? console.log(moment(todo.endTime).diff(todo.startTime, 'seconds')) : null
+        todo.done
+          ? setDoneTodos([
+            ...doneTodos,
+            {
+              title: todo.title,
+              timeTaken: moment(todo.endTime).diff(
+                moment(todo.startTime),
+                'seconds'
+              ),
+            },
+          ])
+          : null
+      );
+    }
+  };
 
   useEffect(() => {
     setLocalTodos();
-    let todoLocal = JSON.parse(localStorage.getItem('todos'));
-    todoLocal.map(todo => (
-      // todo.done ? console.log(moment(todo.endTime).diff(todo.startTime, 'seconds')) : null
-      todo.done ? setDoneTodos([...doneTodos, { title: todo.title, timeTaken: moment(todo.endTime).diff(moment(todo.startTime), 'seconds') }]) : null
-    ))
-  }, [todos])
+  }, [todos]);
 
   useEffect(() => {
     getLocalTodos();
-  }, [])
-
+  }, []);
 
   return (
     <>
       <Row align="middle" justify="center">
-        <Col span={12} >
-          <Row justify="center" style={{ 'marginTop': '5%' }}>
-            <Typography.Title >
-              ToDo
-            </Typography.Title>
+        <Col span={12}>
+          <Row justify="center" style={{ marginTop: '5%' }}>
+            <Typography.Title>ToDo</Typography.Title>
           </Row>
           <Row>
             <InputForm addTodo={addTodo} />
           </Row>
-          <TodoItem
-            todos={todos}
-            onDelete={handleDelete}
-            onDone={handleDone}
-          />
+          <TodoItem todos={todos} onDelete={handleDelete} onDone={handleDone} />
         </Col>
       </Row>
     </>
